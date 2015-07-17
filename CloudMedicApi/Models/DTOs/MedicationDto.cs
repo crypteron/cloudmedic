5f8b39d7d7938ce2ed9b5eb5fde25edd1cdff990
Jeffrey Gao <Jeffrey.Gao@crypteron.com>
2015-07-17 14:14:11 -0700

    Created new Dto for medications

---
+++ CloudMedicApi/Models/DTOs/MedicationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloudMedicApi.Controllers
+{
+    /// <summary>
+    /// This is our DTO from the front end
+    /// </summary>
+    public class MedicationDto
+    {
+        public Guid MedicationId { get; set; }
+
+        public string GenericName { get; set; }
+
+        public string Code { get; set; }
+    }
+}
